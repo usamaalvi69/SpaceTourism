a0319aeef224a87ae78f686a7c2a65ccbb75d48d
UsamaNawaz <usamanawaz659@gmail.com>
2025-02-12 16:06:26 +0500

    feat: header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 import "./header.css";
+import logo from "../../assets/shared/logo.svg";
 export const Header = () => {
   return (
-    <div className='nav-header'>
-        <h1>Header</h1>
+    <div className="nav-header">
+      <img src={logo} alt="Logo" className="logo" />
+      <nav></nav>
     </div>
-  )
-}
+  );
+};
