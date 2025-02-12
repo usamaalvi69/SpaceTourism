38b337e66cd5bdc8c196342ea8be59b17c5e6204
UsamaNawaz <usamanawaz659@gmail.com>
2025-02-12 19:37:34 +0500

    add link to logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,9 @@ import { NavLink } from "react-router-dom";
 export const Header = () => {
   return (
     <div className="nav-header">
-      <img src={logo} alt="Logo" className="logo" />
+      <NavLink to="/">
+        <img src={logo} alt="Logo" className="logo" />
+      </NavLink>
       <nav className="primary-navigation" id="primary-navigation">
         <ul aria-label="Primary" role="list" className="nav-list nav-text">
           <NavLink to="/" className="nav-item">
